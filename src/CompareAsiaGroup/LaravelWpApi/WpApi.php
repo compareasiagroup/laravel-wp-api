1971f0a2077c04df47cc325a910e424003332d4b
Mark Thompson <mark@compareasiagroup.com>
2015-06-18 00:38:25 +0800

    Accept parameters to change query parameters sent to WP-API This is to support custom post type, categories, tags

--- src/CompareAsiaGroup/LaravelWpApi/WpApi.php
+++ src/CompareAsiaGroup/LaravelWpApi/WpApi.php
@@ -33,9 +33,7 @@ class WpApi
             'posts',
             [
                 'page' => $page,
-                'filter' => [
-                    'posts_per_page' => $opts['posts_per_page']
-                ]
+                'filter' => $opts
             ]
         ));
     }
@@ -49,9 +47,7 @@ class WpApi
             [
                 'type' => 'page',
                 'page' => $page,
-                'filter' => [
-                    'posts_per_page' => $opts['posts_per_page']
-                ]
+                'filter' => $opts
             ]
         ));
     }
@@ -66,93 +62,85 @@ class WpApi
         return $this->_get('posts', ['type' => 'page', 'filter' => ['name' => $slug]]);
     }
 
-    public function categories()
+    public function categories($options=[])
     {
-        return $this->_resultsCollection($this->_get('taxonomies/category/terms'));
+        $segment = (isset($options['segment'])) ? $options['segment'] : 'category';
+        return $this->_resultsCollection($this->_get('taxonomies/' . $segment . '/terms'));
     }
 
-    public function tags()
+    public function tags($options=[])
     {
-        return $this->_resultsCollection($this->_get('taxonomies/post_tag/terms'));
+        $segment = (isset($options['segment'])) ? $options['segment'] : 'post_tag';
+        return $this->_resultsCollection($this->_get('taxonomies/' . $segment . '/terms'));
     }
 
     public function category_posts($slug, $page = null, $options = [])
     {
+        $options['category_name'] = $slug;
         $opts = $this->extendDefaults($options);
 
         return $this->_resultsCollection($this->_get(
             'posts',
             [
                 'page' => $page,
-                'filter' => [
-                    'category_name' => $slug,
-                    'posts_per_page' => $opts['posts_per_page']
-                ]
+                'filter' => $opts
             ]
         ));
     }
 
     public function author_posts($name, $page = null, $options = [])
     {
+        $options['author_name'] = $name;
         $opts = $this->extendDefaults($options);
 
         return $this->_resultsCollection($this->_get(
             'posts',
             [
                 'page' => $page,
-                'filter' => [
-                    'author_name' => $name,
-                    'posts_per_page' => $opts['posts_per_page']
-                ]
+                'filter' => $opts
             ]
         ));
     }
 
     public function tag_posts($tags, $page = null, $options = [])
     {
+        $options['tag'] = $tags;
         $opts = $this->extendDefaults($options);
 
         return $this->_resultsCollection($this->_get(
             'posts',
             [
                 'page' => $page,
-                'filter' => [
-                    'tag' => $tags,
-                    'posts_per_page' => $opts['posts_per_page']
-                ]
+                'filter' => $opts
             ]
         ));
     }
 
     public function search($query, $page = null, $options = [])
     {
+        $options['s'] = $query;
         $opts = $this->extendDefaults($options);
 
         return $this->_resultsCollection($this->_get(
             'posts',
             [
                 'page' => $page,
-                'filter' => [
-                    's' => $query,
-                    'posts_per_page' => $opts['posts_per_page']
-                ]
+                'filter' => $opts
             ]
         ));
     }
 
     public function archive($year, $month, $page = null, $options = [])
     {
+        $options['year'] = $year;
+        $options['monthnum'] = $month;
         $opts = $this->extendDefaults($options);
 
         return $this->_resultsCollection($this->_get(
             'posts',
             [
                 'page' => $page,
-                'filter' => [
-                    'year' => $year,
-                    'monthnum' => $month,
-                    'posts_per_page' => $opts['posts_per_page']
-                ]
+                'filter' => $opts
             ]
         ));
     }
